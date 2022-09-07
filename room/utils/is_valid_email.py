9c38492d253bc49b6a3f20ceab043860de442ecf
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-07 17:01:32 +0430

    create script to verify that string is email or username

---
+++ room/utils/is_valid_email.py
@@ -0,0 +1,10 @@
+import re
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+
+def is_valid(email):
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
