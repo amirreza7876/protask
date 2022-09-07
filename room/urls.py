29e520960a0ba907361fa9b4cf8bd8938f45f7bc
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-07 16:59:52 +0430

    add invitation view and update roomstring to request string

--- room/urls.py
+++ room/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import RoomListApi, user_request, room_request, change_request_status, RoomDetailApi, create_room
+from .views import RoomListApi, user_request, room_request, change_request_status, RoomDetailApi, create_room, \
+    user_invitation
 
 # router = DefaultRouter()
 # router.register('mine', RoomViewSet, basename='rooms')
@@ -10,7 +11,8 @@ urlpatterns = [
     path('mine/', RoomListApi.as_view(), name='my-rooms'),
     path('mine/<int:pk>/', RoomDetailApi.as_view(), name='my-rooms'),
     path('join-requests/', user_request, name='user-request'),
-    path('room-requests/<int:room_id>/<str:room_string>/', room_request, name='room-request'),
+    path('join-invites/', user_invitation, name='user-invites'),
+    path('room-requests/<int:room_id>/<str:request_string>/', room_request, name='room-request'),
     path('request/', change_request_status, name='change-status'),
     path('create/', create_room, name='create-room')
 
