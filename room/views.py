fdf1fc6256821eade8198e9ebd45b0681c2d13ff
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-07 17:01:08 +0430

    add is authenticated permission to userrequest view, create invite view

--- room/views.py
+++ room/views.py
@@ -1,10 +1,12 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, get_object_or_404
-from room.serializers import RoomSerializer, JoinRequestSerializer
+from room.serializers import RoomSerializer, JoinRequestSerializer, InviteRequestSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+
+from room.utils.is_valid_email import is_valid
 from user.serializer import UserSerializer
-from room.models import JoinRequest, Room
+from room.models import JoinRequest, Room, InviteRequest
 from room.permissions import IsMember
 from user.models import CustomUser
 from rest_framework import status
@@ -35,45 +37,72 @@ class RoomDetailApi(RetrieveAPIView):
 
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def user_request(request):
     if request.method == 'GET':
-        if not request.user.is_anonymous:
-            requests = JoinRequest.objects.filter(from_user=request.user)
-            serializer = JoinRequestSerializer(data=requests, many=True)
-            serializer.is_valid()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response({"data": "login first"}, status=status.HTTP_401_UNAUTHORIZED)
+        requests = JoinRequest.objects.filter(from_user=request.user)
+        serializer = JoinRequestSerializer(data=requests, many=True)
+        serializer.is_valid()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == 'POST':
-        # room members cant request again to the same room
-        if not request.user.is_anonymous:
-            user = request.user
-            request_string = request.data['request_string']
-            room = get_object_or_404(Room, request_string=request_string)
-            room_members = room.members.all()
+        user = request.user
+        request_string = request.data['request_string']
+        room = get_object_or_404(Room, request_string=request_string)
+        room_members = room.members.all()
 
-            request_object = JoinRequest.objects.get_or_create(from_user=user, for_room=room)
-            request_object_model = request_object[0]
-            request_status = request_object_model.status
-            request_already_exist = request_object[1]
-            if user not in room_members:
-                if not request_already_exist and request_status == 'r':
-                    request_object_model.status = 'p'
-                    request_object_model.save()
-                    return Response({"msg": "request sent"}, status=status.HTTP_201_CREATED)
-                if request_already_exist:
-                    return Response({"msg": "request sent"}, status=status.HTTP_201_CREATED)
-                return Response({'msg': 'already requested to join'}, status=status.HTTP_400_BAD_REQUEST)
-            return Response({"msg": "already member"}, status=status.HTTP_400_BAD_REQUEST)
-        return Response({'msg': "login first"}, status=status.HTTP_401_UNAUTHORIZED)
+        request_object = JoinRequest.objects.get_or_create(from_user=user, for_room=room)
+        request_object_model = request_object[0]
+        request_status = request_object_model.status
+        request_already_exist = request_object[1]
+        if user not in room_members:
+            if not request_already_exist and request_status == 'r':
+                request_object_model.status = 'p'
+                request_object_model.save()
+                return Response({"msg": "request sent"}, status=status.HTTP_201_CREATED)
+            if request_already_exist:
+                return Response({"msg": "request sent"}, status=status.HTTP_201_CREATED)
+            return Response({'msg': 'already requested to join'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"msg": "already member"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def user_invitation(request):
+    user = request.user
+
+    if request.method == 'GET':
+        invites = InviteRequest.objects.filter(for_user=user)
+        serializer = InviteRequestSerializer(data=invites, many=True)
+        serializer.is_valid()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        request_string = request.data['requestString']
+        username_or_email = request.data['usernameOrEmail']
+        room = get_object_or_404(Room, request_string=request_string)
+        room_members = room.members.all()
+
+        if user not in room_members:
+            return Response({'error': "not member"}, status=status.HTTP_403_FORBIDDEN)
+
+        # is_valid() returns True if format of username_or_email matches with email
+        is_email = is_valid(username_or_email)
+        target_user = get_object_or_404(CustomUser, email=username_or_email) if is_email else \
+            get_object_or_404(CustomUser, username=username_or_email)
+
+        if target_user in room_members:
+            return Response({'error': "already member"}, status=status.HTTP_400_BAD_REQUEST)
+
+        InviteRequest.objects.create(from_room=room, for_user=target_user)
+        return Response({'data': 'invitation sent'}, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET'])
-def room_request(request, room_id, room_string):
-    room = get_object_or_404(Room, request_string=room_string, id=room_id)
+def room_request(request, room_id, request_string):
+    room = get_object_or_404(Room, request_string=request_string, id=room_id)
     room_members = room.members.all()
-    if request.user in room_members:
+    user = request.user
+    if user in room_members:
         room_requests = JoinRequest.objects.filter(for_room=room)
         serializer = JoinRequestSerializer(room_requests, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
