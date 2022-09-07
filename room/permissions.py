e7012b02bb262741d0cf0991e2caabe7e4b1562d
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-07 16:59:01 +0430

    create invite serializer and make roomserializer fields dynamic

--- room/permissions.py
+++ room/permissions.py
@@ -15,17 +15,17 @@ class IsMember(permissions.BasePermission):
         return False
 
 
-class IsNotMember(permissions.BasePermission):
-    edit_methods = ("POST", )
-
-    def has_permission(self, request, view):
-        print('has perm')
-        if request.user.is_authenticated:
-            return True
-
-    def has_object_permission(self, request, view, obj):
-        print('has obj perm')
-        user = request.user
-        if user not in obj.members.all():
-            return True
-        return False
+# class IsNotMember(permissions.BasePermission):
+#     edit_methods = ("POST", )
+#
+#     def has_permission(self, request, view):
+#         print('has perm')
+#         if request.user.is_authenticated:
+#             return True
+#
+#     def has_object_permission(self, request, view, obj):
+#         print('has obj perm')
+#         user = request.user
+#         if user not in obj.members.all():
+#             return True
+#         return False
--- room/serializers.py
+++ room/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from room.models import Room, JoinRequest
+from room.models import Room, JoinRequest, InviteRequest
 from user.serializer import UserSerializer
 
 
@@ -8,12 +8,25 @@ class RoomSerializer(serializers.ModelSerializer):
     leader = UserSerializer()
     is_owner = serializers.SerializerMethodField()
 
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+
+        # Instantiate the superclass normally
+        super().__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
     class Meta:
         model = Room
         fields = "__all__"
 
     def get_is_owner(self, obj):
-        print(self.context, 'look here')
         try:
             return obj.leader == self.context['request'].user
         except KeyError:
@@ -26,4 +39,13 @@ class JoinRequestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = JoinRequest
+        fields = "__all__"
+
+
+class InviteRequestSerializer(serializers.ModelSerializer):
+    for_user = UserSerializer()
+    from_room = RoomSerializer(fields=('name', 'id'))
+
+    class Meta:
+        model = InviteRequest
         fields = "__all__"
