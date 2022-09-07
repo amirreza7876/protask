e399d9a6d0fad6454d00cca409de642550da5534
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-07 16:57:38 +0430

    register invites to admin

--- room/admin.py
+++ room/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from room.models import Room, JoinRequest
+from room.models import Room, JoinRequest, InviteRequest
 
 admin.site.register(Room)
 admin.site.register(JoinRequest)
+admin.site.register(InviteRequest)
