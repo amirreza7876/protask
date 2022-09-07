86c9360d0892d9275cd88e59d90d44d2bff563cb
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-08 01:58:30 +0430

    add room invite routes

--- room/urls.py
+++ room/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import RoomListApi, user_request, room_request, change_request_status, RoomDetailApi, create_room, \
-    user_invitation
+from .views import RoomListApi, user_request, room_request_list, change_request_status, RoomDetailApi, create_room, \
+    user_invitation, change_invite_status, room_invite_list
 
 # router = DefaultRouter()
 # router.register('mine', RoomViewSet, basename='rooms')
@@ -12,8 +12,10 @@ urlpatterns = [
     path('mine/<int:pk>/', RoomDetailApi.as_view(), name='my-rooms'),
     path('join-requests/', user_request, name='user-request'),
     path('join-invites/', user_invitation, name='user-invites'),
-    path('room-requests/<int:room_id>/<str:request_string>/', room_request, name='room-request'),
-    path('request/', change_request_status, name='change-status'),
+    path('room-requests/<int:room_id>/<str:request_string>/', room_request_list, name='room-request'),
+    path('room-invites/<int:room_id>/<str:request_string>/', room_invite_list, name='room-request'),
+    path('request/', change_request_status, name='change-request-status'),
+    path('invite/', change_invite_status, name='change-invite-status'),
     path('create/', create_room, name='create-room')
 
 ]
