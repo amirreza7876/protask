6088523468982b23bde9d71bc490ca00bcfffeb0
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-09 01:01:05 +0430

    create color field for room ui background color icons

--- room/models.py
+++ room/models.py
@@ -8,6 +8,7 @@ class Room(models.Model):
     leader = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='rooms_own', null=True)
     members = models.ManyToManyField(CustomUser, related_name='user_rooms', blank=True)
     request_string = models.CharField(max_length=100, null=True, blank=True, unique=True)
+    color = models.CharField(max_length=7, default="#000000")
 
     def __str__(self):
         return self.name
