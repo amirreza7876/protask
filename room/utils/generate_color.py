44d4bb19c9d58154db1ac7196063ac6792ca442f
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-09 01:02:45 +0430

    create script to generate random hex colors

---
+++ room/utils/generate_color.py
@@ -0,0 +1,8 @@
+import random
+
+
+def generate_color():
+    random_number = random.randint(0, 16777215)
+    hex_number = str(hex(random_number))
+    hex_number = '#' + hex_number[2:]
+    return hex_number
