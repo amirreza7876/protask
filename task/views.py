3be7c8fc6409ef14f13b0f7184428f48b2dd29cb
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-10 03:15:43 +0430

    create apiview for board task list

--- task/views.py
+++ task/views.py
@@ -1,11 +1,27 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+
+from room.models import Room
+from task.models import Task
 from task.serializers import TaskSerializer
 
 
-class TaskViewSet(ModelViewSet):
+class UserTaskViewSet(ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return self.request.user.user_tasks.all()
+
+
+class RoomTaskViewSet(ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    # queryset = Task.objects.all()
+    def get_queryset(self):
+        request_string = self.request.query_params['requestString']
+        room_id = self.request.query_params['roomId']
+        room = get_object_or_404(Room, request_string=request_string, id=room_id)
+        return room.room_tasks.all()
