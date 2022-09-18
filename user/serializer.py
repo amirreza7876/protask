5f314fb8bebb3f3e3206618d32adc1cb8cb0caa1
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-19 02:08:19 +0430

    remove commented lines

--- user/serializer.py
+++ user/serializer.py
@@ -5,14 +5,12 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
-
     return {
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
 
 
-# Register serializer
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
@@ -31,33 +29,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'email', 'username']
-        # id
-        # password
-        # last_login
-        # phone_number
-        # email
-        # username
-        # firstname
-        # lastname
-        # is_active
-        # is_superuser
-        # is_staff
-        # groups
-        # user_permissions
-
-# class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-#     @classmethod
-#     def get_token(cls, user):
-#         token = super().get_token(user)
-#         token['username'] = user.username
-#         return token
-
-
-# class MyTokenVerifySerializer(TokenVerifySerializer):
-#     def validate(self, attrs):
-#         token = attrs['token']
-#         print(type(token))
-#         user = Token.objects.get(key=token)
-#         # print(user)
-#         super().validate(attrs)
-#         return {'code': 'token is valid'}
