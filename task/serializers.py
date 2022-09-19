d1ddbe50ad24c30d935fda15c1152b27c358ec40
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-20 03:13:53 +0430

    modify serializer data for user

--- task/serializers.py
+++ task/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+
+from user.serializer import UserSerializer
 from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Task
         fields = "__all__"
