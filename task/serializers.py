6caf9ed2f00f964130b7b67df2de083fdae1e557
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-30 16:30:57 +0330

    add endpoints for create and update single task

--- task/serializers.py
+++ task/serializers.py
@@ -6,6 +6,7 @@ from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+
     class Meta:
         model = Task
         fields = "__all__"
--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import UserTaskViewSet, RoomTaskViewSet
+from .views import UserTaskViewSet, RoomTaskViewSet, create_new_task, update_task
 
 router = DefaultRouter()
 router.register('user-tasks', UserTaskViewSet, basename='user-tasks')
 router.register('room-tasks', RoomTaskViewSet, basename='room-tasks')
 urlpatterns = router.urls
+
+urlpatterns +=[
+    path('create/', create_new_task, name='create-task'),
+    path('update/', update_task, name='create-task'),
+
+]
