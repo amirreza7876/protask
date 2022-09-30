46634e3886452d4c3569ceb713ed1d15a5830153
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-30 16:31:19 +0330

    create update and create endpoint view

--- task/views.py
+++ task/views.py
@@ -1,11 +1,16 @@
-from rest_framework.decorators import action
+from django.forms import model_to_dict
+from rest_framework import status
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from room.models import Room
+from room.permissions import IsOwner
 from task.models import Task
 from task.serializers import TaskSerializer
+from user.models import CustomUser
 
 
 class UserTaskViewSet(ModelViewSet):
@@ -21,6 +26,9 @@ class RoomTaskViewSet(ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        serializer.save()
+
     def get_queryset(self):
         request_string = self.request.query_params['requestString']
         room_id = self.request.query_params['roomId']
@@ -30,6 +38,44 @@ class RoomTaskViewSet(ModelViewSet):
     # @action(detail=True, methods=['get'], url_path='get_room_tasks')
     # def get_tasks(self, request, pk=None):
     #     print(request.user)
-        # room_id = self.request.query_params['roomId']
-        # room = get_object_or_404(Room, request_string=request_string, id=room_id)
-        # return room.room_tasks.all()
+    # room_id = self.request.query_params['roomId']
+    # room = get_object_or_404(Room, request_string=request_string, id=room_id)
+    # return room.room_tasks.all()
+
+
+@api_view(['POST'])
+def create_new_task(request):
+    user = request.data.get('user')
+    duration = request.data.get('duration')
+    title = request.data.get('title')
+    difficulty = request.data.get('difficulty')
+    priority = request.data.get('priority')
+    board_id = request.data.get('id')
+
+    user = get_object_or_404(CustomUser, username=user)
+    board = get_object_or_404(Room, id=board_id)
+
+    new = Task(owner=board, user=user, duration=duration, title=title, difficulty=difficulty,
+               priority=priority)
+    new.status = 'dg'
+    new.save()
+    return Response({'msg': 'created'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['PATCH'])
+@permission_classes([IsOwner, ])
+def update_task(request):
+    task_id = request.data.get('id')
+    value = request.data.get('value')
+    field = request.data.get('field')
+    task = get_object_or_404(Task, id=task_id)
+
+    if field == 'user':
+        user = get_object_or_404(CustomUser, username=value)
+        task.user = user
+        task.save()
+    else:
+        task.__setattr__(field, value)
+        task.save()
+
+    return Response({'msg': "updated"})
