d2d4a5f8a072250bf31a7d4405fc6e0423b176d9
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-06 22:00:59 +0330

    create serializer for phase model

--- task/serializers.py
+++ task/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-
 from user.serializer import UserSerializer
-from .models import Task
+from .models import Task, Phase
+
+
+class PhaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Phase
+        fields = '__all__'
 
 
 class TaskSerializer(serializers.ModelSerializer):
