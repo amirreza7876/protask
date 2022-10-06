f2ab206867129742659330f12d73b521ce7784fd
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-06 22:03:35 +0330

    create view for phase handling; done field update; add phase to task create view

--- task/views.py
+++ task/views.py
@@ -8,8 +8,8 @@ from rest_framework.viewsets import ModelViewSet
 
 from room.models import Room
 from room.permissions import IsOwner
-from task.models import Task
-from task.serializers import TaskSerializer
+from task.models import Task, Phase
+from task.serializers import TaskSerializer, PhaseSerializer
 from user.models import CustomUser
 
 
@@ -32,15 +32,10 @@ class RoomTaskViewSet(ModelViewSet):
     def get_queryset(self):
         request_string = self.request.query_params['requestString']
         room_id = self.request.query_params['roomId']
+        phase_name = self.request.query_params['phaseName']
+        phase_id = self.request.query_params['phaseId']
         room = get_object_or_404(Room, request_string=request_string, id=room_id)
-        return room.room_tasks.all()
-
-    # @action(detail=True, methods=['get'], url_path='get_room_tasks')
-    # def get_tasks(self, request, pk=None):
-    #     print(request.user)
-    # room_id = self.request.query_params['roomId']
-    # room = get_object_or_404(Room, request_string=request_string, id=room_id)
-    # return room.room_tasks.all()
+        return room.room_tasks.filter(phase_id=phase_id, phase__name=phase_name)
 
 
 @api_view(['POST'])
@@ -51,12 +46,13 @@ def create_new_task(request):
     difficulty = request.data.get('difficulty')
     priority = request.data.get('priority')
     board_id = request.data.get('id')
-
+    phase_object = request.data.get('selectedPhase')
+    phase = get_object_or_404(Phase, board_id=board_id, id=phase_object['id'])
     user = get_object_or_404(CustomUser, username=user)
     board = get_object_or_404(Room, id=board_id)
 
     new = Task(owner=board, user=user, duration=duration, title=title, difficulty=difficulty,
-               priority=priority)
+               priority=priority, phase=phase)
     new.status = 'dg'
     new.save()
     return Response({'msg': 'created'}, status=status.HTTP_201_CREATED)
@@ -69,6 +65,13 @@ def update_task(request):
     value = request.data.get('value')
     field = request.data.get('field')
     task = get_object_or_404(Task, id=task_id)
+    if field == 'done':
+        if value:
+            task.done = value
+            task.status = 'd'
+        else:
+            task.done = value
+            task.status = 'dg'
 
     if field == 'user':
         user = get_object_or_404(CustomUser, username=value)
@@ -79,3 +82,19 @@ def update_task(request):
         task.save()
 
     return Response({'msg': "updated"})
+
+
+class RoomPhaseViewSet(ModelViewSet):
+    lookup_field = 'id'
+    serializer_class = PhaseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        board = get_object_or_404(Room, id=self.request.data.get('id'))
+        serializer.save(board=board)
+
+    def get_queryset(self):
+        request_string = self.request.query_params['requestString']
+        room_id = self.request.query_params['roomId']
+        room = get_object_or_404(Room, request_string=request_string, id=room_id)
+        return room.room_phases.all()
