283552d418a76ab6620afe0f27d80f35a9a67fa7
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-06 22:00:35 +0330

    create phase model

--- task/models.py
+++ task/models.py
@@ -4,8 +4,17 @@ from user.models import CustomUser
 from room.models import Room
 
 
+class Phase(models.Model):
+    name = models.CharField(max_length=250, null=True)
+    board = models.ForeignKey(Room, on_delete=models.CASCADE, null=True, related_name='room_phases')
+
+    def __str__(self):
+        return f'phase {self.name} / board {self.board.name}'
+
+
 class Task(models.Model):
     owner = models.ForeignKey(Room, on_delete=models.CASCADE, null=True, related_name='room_tasks')
+    phase = models.ForeignKey(Phase, on_delete=models.CASCADE, null=True, related_name='phase_tasks')
     user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='user_tasks')
     title = models.CharField(max_length=250)
     done = models.BooleanField(default=False)
