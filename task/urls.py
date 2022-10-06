22b33287a8f63c497c93ab1d85f656fa0fef026d
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-06 22:01:25 +0330

    create route for phase handling

--- task/urls.py
+++ task/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import UserTaskViewSet, RoomTaskViewSet, create_new_task, update_task
+from .views import UserTaskViewSet, RoomTaskViewSet, create_new_task, update_task, RoomPhaseViewSet
 
 router = DefaultRouter()
 router.register('user-tasks', UserTaskViewSet, basename='user-tasks')
 router.register('room-tasks', RoomTaskViewSet, basename='room-tasks')
+router.register('room-phases', RoomPhaseViewSet, basename='room-phases')
 urlpatterns = router.urls
 
-urlpatterns +=[
+urlpatterns += [
     path('create/', create_new_task, name='create-task'),
     path('update/', update_task, name='create-task'),
-
 ]
