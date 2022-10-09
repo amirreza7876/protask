c7b373ffc5186dfaea6574b9df4551781885b87d
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-10 00:50:42 +0330

    comment phase creator signal for now

--- room/signals.py
+++ room/signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from room.models import Room
+from task.models import Phase
 from room.utils.generate_color import generate_color
 from room.utils.random_string import get_random_string
 
@@ -15,3 +16,9 @@ def create_request_string(sender, instance, *args, **kwargs):
 def create_background_color(sender, instance, *args, **kwargs):
     if instance.color == '#000000':
         instance.color = generate_color()
+
+
+# @receiver(post_save, sender=Room)
+# def create_default_phases(sender, instance, *args, **kwargs):
+#     Phase.objects.create(name='Front-End', board=instance)
+#     Phase.objects.create(name='Back-End', board=instance)
