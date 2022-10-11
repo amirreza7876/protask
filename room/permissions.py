b1900e231c10446cb789dce5f41fcc0cf5c17762
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-12 02:13:52 +0330

    django migrations

--- room/permissions.py
+++ room/permissions.py
@@ -1,8 +1,11 @@
 from rest_framework import permissions
+from rest_framework.generics import get_object_or_404
+
+from room.models import Room
 
 
 class IsMember(permissions.BasePermission):
-    edit_methods = ("GET",)
+    edit_methods = ("GET", 'DELETE')
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
@@ -16,7 +19,7 @@ class IsMember(permissions.BasePermission):
 
 
 class IsOwner(permissions.BasePermission):
-    edit_methods = ("POST",)
+    edit_methods = ("POST", "DELETE")
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
@@ -26,5 +29,23 @@ class IsOwner(permissions.BasePermission):
         user = request.user
         room_leader = obj.leader
         if user == room_leader:
+            return True
+        return False
+
+
+class IsMemberFunctional(permissions.BasePermission):
+    def has_permission(self, request, view):
+        room_id = request.data.get('roomId')
+        room_object = get_object_or_404(Room, id=room_id)
+        if room_object in request.user.user_rooms.all():
+            return True
+        return False
+
+
+class IsOwnerFunctional(permissions.BasePermission):
+    def has_permission(self, request, view):
+        room_id = request.data.get('roomId')
+        room_object = get_object_or_404(Room, id=room_id)
+        if room_object in request.user.rooms_own.all():
             return True
         return False
